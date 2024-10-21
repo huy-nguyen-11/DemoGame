f60db61c8a5c3dda8198b8caa75b979c65877935
Huy MG <imhuy112k@gmail.com>
2024-10-21 18:19:40 +0700

    rotate furniture

--- Assets/Scripts/Game2/Furniture.cs
+++ Assets/Scripts/Game2/Furniture.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,85 +7,6 @@ using UnityEngine.EventSystems;
 
 public class Furniture : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-    //public Transform targetPos;
-    //public Game2Manager game2Manager;
-    //Vector3 _startPos;
-    //Vector3 offset;
-
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    GetComponent<Button>().onClick.AddListener(Moving);
-    //}
-
-    //void Moving()
-    //{
-    //    int rate = Random.RandomRange(1, 4);
-    //    if (rate == 1)
-    //    {
-    //        AudioManager.instance.PlaySFX("welldone");
-    //    }
-    //    else if (rate == 2)
-    //    {
-    //        AudioManager.instance.PlaySFX("goodjob");
-    //    }
-    //    else if (rate == 3)
-    //    {
-    //        AudioManager.instance.PlaySFX("correct");
-    //    }
-    //    else
-    //    {
-    //        AudioManager.instance.PlaySFX("excellent");
-    //    }
-
-    //    transform.DOMove(targetPos.position, 0.8f).SetEase(Ease.OutBack).OnComplete(() =>
-    //    {
-    //        //AudioManager.instance.PlaySFX("spawn");
-    //        float posX = Random.Range(-2f, 2f);
-    //        float posY = Random.Range(-1f, 1f);
-    //        transform.DOMove(new Vector3(transform.position.x + posX, transform.position.y + 3 + posY, 0), 0.5f).SetEase(Ease.InBack);
-
-    //        float randomRotationZ = Random.Range(0, 2) == 0 ? -20f : 20f;
-    //        transform.DORotate(new Vector3(0, 0, randomRotationZ), 0.5f).OnComplete(() =>
-    //        {
-    //            GetComponent<Button>().enabled = false;
-    //            game2Manager.Creating();
-    //        });
-    //    });
-    //}
-
-    //CanvasGroup canvasGroup;
-    //public string destinationTag = "DropArea";
-
-    //void Awake()
-    //{
-    //    if (gameObject.GetComponent<CanvasGroup>() == null)
-    //        gameObject.AddComponent<CanvasGroup>();
-    //    canvasGroup = gameObject.GetComponent<CanvasGroup>();
-    //}
-
-    //public void OnDrag(PointerEventData eventData)
-    //{
-    //    transform.localPosition = Input.mousePosition + offset;
-    //}
-
-    //public void OnPointerDown(PointerEventData eventData)
-    //{
-    //    offset = transform.localPosition - Input.mousePosition;
-    //    canvasGroup.alpha = 0.5f;
-    //    canvasGroup.blocksRaycasts = false;
-    //}
-
-    //public void OnPointerUp(PointerEventData eventData)
-    //{
-    //    RaycastResult raycastResult = eventData.pointerCurrentRaycast;
-    //    if (raycastResult.gameObject?.tag == destinationTag)
-    //    {
-    //        transform.localPosition = raycastResult.gameObject.transform.localPosition;
-    //    }
-    //    canvasGroup.alpha = 1;
-    //    canvasGroup.blocksRaycasts = true;
-    //}
     public Transform targetPos;
     public Game2Manager game2Manager;
     Vector3 _startPos;
@@ -96,46 +17,11 @@ public class Furniture : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoin
     // Start is called before the first frame update
     void Start()
     {
-        //GetComponent<Button>().onClick.AddListener(Moving);
         _startPos = transform.localPosition;  // Store the initial position
         isSelectDrag = false;
         isEnableDrag = true;
     }
 
-    //void Moving()
-    //{
-    //    int rate = Random.Range(1, 4);
-    //    if (rate == 1)
-    //    {
-    //        AudioManager.instance.PlaySFX("welldone");
-    //    }
-    //    else if (rate == 2)
-    //    {
-    //        AudioManager.instance.PlaySFX("goodjob");
-    //    }
-    //    else if (rate == 3)
-    //    {
-    //        AudioManager.instance.PlaySFX("correct");
-    //    }
-    //    else
-    //    {
-    //        AudioManager.instance.PlaySFX("excellent");
-    //    }
-
-    //    transform.DOMove(targetPos.position, 0.8f).SetEase(Ease.OutBack).OnComplete(() =>
-    //    {
-    //        float posX = Random.Range(-2f, 2f);
-    //        float posY = Random.Range(-1f, 1f);
-    //        transform.DOMove(new Vector3(transform.position.x + posX, transform.position.y + 3 + posY, 0), 0.5f).SetEase(Ease.InBack);
-
-    //        float randomRotationZ = Random.Range(0, 2) == 0 ? -20f : 20f;
-    //        transform.DORotate(new Vector3(0, 0, randomRotationZ), 0.5f).OnComplete(() =>
-    //        {
-    //            GetComponent<Button>().enabled = false;
-    //            game2Manager.Creating();
-    //        });
-    //    });
-    //}
 
     CanvasGroup canvasGroup;
     public string destinationTag = "DropArea";
@@ -147,26 +33,58 @@ public class Furniture : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoin
         canvasGroup = gameObject.GetComponent<CanvasGroup>();
     }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (game2Manager.isDragObject && isEnableDrag)
-        {
-            transform.localPosition = Input.mousePosition + offset;
-        }
-
-    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         if (game2Manager.isDragObject && isEnableDrag)
         {
-            //_startPos = transform.localPosition;  // Store the initial position
-            offset = transform.localPosition - Input.mousePosition;
+
+
+            //offset = transform.localPosition - Input.mousePosition;
+            //canvasGroup.alpha = 0.75f;
+            //canvasGroup.blocksRaycasts = false;
+
+
+            RectTransform rectTransform = GetComponent<RectTransform>();
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                transform.parent as RectTransform,
+                Input.mousePosition,
+                eventData.pressEventCamera,
+                out Vector2 localPoint
+            );
+            Vector3 newLocalPoint = new Vector3(localPoint.x, localPoint.y, transform.localPosition.z); 
+            offset = transform.localPosition - newLocalPoint;
+
             canvasGroup.alpha = 0.75f;
             canvasGroup.blocksRaycasts = false;
         }
 
     }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (game2Manager.isDragObject && isEnableDrag)
+        {
+            //transform.localPosition = Input.mousePosition + offset;
+
+
+            RectTransform rectTransform = GetComponent<RectTransform>();
+
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                transform.parent as RectTransform,
+                Input.mousePosition,
+                eventData.pressEventCamera,
+                out Vector2 localPoint
+            );
+
+
+            Vector3 newLocalPoint = new Vector3(localPoint.x, localPoint.y, transform.localPosition.z);
+            transform.localPosition = newLocalPoint + offset;
+        }
+
+    }
+
 
     public void OnPointerUp(PointerEventData eventData)
     {
@@ -199,7 +117,13 @@ public class Furniture : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoin
                     {
                         DOVirtual.DelayedCall(0.3f, () =>
                         {
-                            transform.DOLocalMove(Vector3.zero, 0.5f).SetEase(Ease.InOutBack).OnComplete(() =>
+                            int num = Random.Range(-5, 5);   
+                            float posX = Random.Range(-200f, 200f);
+                            float posY = Random.Range(-1f, 1f);
+                            float randomRotationZ = Random.Range(0, 2) == 0 ? -20f : 20f;
+                            transform.DORotate(new Vector3(0, 0, randomRotationZ), 0.5f);
+
+                            transform.DOLocalMove(new Vector3(num * 25 , posY, 0), 0.5f).SetEase(Ease.InOutBack).OnComplete(() =>
                             {
                                 isEnableDrag = false;
                                 game2Manager.Creating();
